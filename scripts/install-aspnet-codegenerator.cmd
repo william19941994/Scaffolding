5af43394e357099e894d07a55a28255a614e664a
Deep Choudhery <decho@microsoft.com>
2021-06-08 11:48:24 -0700

    fixed out of repo execution for scripts

--- scripts/install-aspnet-codegenerator.cmd
+++ scripts/install-aspnet-codegenerator.cmd
@@ -1,12 +1,21 @@
 set VERSION=6.0.0-dev
-set DEFAULT_NUPKG_PATH=%userprofile%/.nuget/packages
+set DEFAULT_NUPKG_PATH=%userprofile%\.nuget\packages
+set NUPKG=artifacts\packages\Debug\Shipping\
+
+rem save current directory path, push it on a stack and switch to it.
+pushd %~dp0
+call cd ..
+
+rem save base project path.
 set SRC_DIR=%cd%
-set NUPKG=artifacts/packages/Debug/Shipping/
+
+rem kill any lingering dotnet.exe's that would interfere with dotnet packing and tool installing.
 call taskkill /f /im dotnet.exe
 call rd /Q /S artifacts
-call dotnet build Scaffolding.slnf
-call dotnet pack Scaffolding.slnf 
-call dotnet tool uninstall -g dotnet-aspnet-codegenerator 
+
+call dotnet pack Scaffolding.slnf
+call dotnet tool uninstall -g dotnet-aspnet-codegenerator
+
 
 call cd %DEFAULT_NUPKG_PATH%
 call rd /Q /S microsoft.visualstudio.web.codegeneration
@@ -18,6 +27,8 @@ call rd /Q /S microsoft.visualstudio.web.codegeneration.templating
 call rd /Q /S microsoft.visualstudio.web.codegeneration.utils
 call rd /Q /S microsoft.visualstudio.web.codegenerators.mvc
 
-call cd  %SRC_DIR%/%NUPKG% 
+call cd  %SRC_DIR%\%NUPKG% 
 call dotnet tool install -g dotnet-aspnet-codegenerator --add-source %SRC_DIR%\%NUPKG% --version %VERSION%
-call cd %SRC_DIR%
+
+rem return to directory at the top of the stack.
+popd
--- scripts/install-msidentity.cmd
+++ scripts/install-msidentity.cmd
@@ -1,15 +1,20 @@
 set VERSION=1.0.0-dev
 set NUPKG=artifacts\packages\Debug\Shipping\
 
+rem save current directory path, push it on a stack and switch to it.
 pushd %~dp0
 call cd ..
+
+rem save base project path.
 set SRC_DIR=%cd%
 
+rem kill any lingering dotnet.exe's that would interfere with dotnet packing and tool installing.
 call taskkill /f /im dotnet.exe
 call rd /Q /S artifacts
 
-call dotnet build MSIdentityScaffolding.slnf
 call dotnet pack MSIdentityScaffolding.slnf
 call dotnet tool uninstall -g Microsoft.dotnet-msidentity
 call dotnet tool install -g Microsoft.dotnet-msidentity --add-source %SRC_DIR%\%NUPKG% --version %VERSION%
+
+rem return to directory at the top of the stack.
 popd
